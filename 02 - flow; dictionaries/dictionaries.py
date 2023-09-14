64d1d118d0dba77f325411857ab835f72fb760ae
Richard Murray <rfm@yorku.ca>
2023-09-14 10:53:53 -0400

    revised examples

--- 02 - flow; dictionaries/dictionaries.py
+++ 02 - flow; dictionaries/dictionaries.py
@@ -1,7 +1,7 @@
 # dictionaries.py  Dictionaries
 
 # - dictionaries are mappings (not sequences)
-# - values are stored under a key
+# - values are stored under a *key*
 # - key can be any immutable type, e.g., number, string, tuple
 # - keys are unique; values do not need to be
 # - (key, value) pairs have no particular order in the dictionary
@@ -9,14 +9,8 @@
 # 1. creating dictionaries
 
 # most common methods
-expt = dict(subject='jfk', time='14-Jan-1960 16:00', condition='validcue')
 expt = {'subject' : 'jfk', 'time' : '14-Jan-1960 16:00', 'condition' : 'validcue'}
-
-# additional methods
-expt = dict([('subject', 'jfk'), ('time', '14-Jan-1960 16:00'), ('condition', 'validcue')])
-expt = dict()
-expt = dict.fromkeys(['subject', 'time', 'condition'])     # create a new dictionary with these keys
-expt = dict.fromkeys(['subject', 'time', 'condition'], '') # last argument is default value
+expt = dict(subject='jfk', time='14-Jan-1960 16:00', condition='validcue')
 
 # 2. basic dictionary operations
 
@@ -30,6 +24,12 @@ expt['rt']               # error ("raises an exception"; KeyError)
 len(expt)                # number of entries
 
 # 3. dictionary methods
+
+# more ways of creating dictionaries
+expt = dict([('subject', 'jfk'), ('time', '14-Jan-1960 16:00'), ('condition', 'validcue')])
+expt = dict()
+expt = dict.fromkeys(['subject', 'time', 'condition'])     # create a new dictionary with these keys
+expt = dict.fromkeys(['subject', 'time', 'condition'], '') # last argument is default value
 
 # set values
 x = expt.setdefault('pcorrect',0.75)  # sets value for key 'pcorrect' if none exists;
@@ -47,7 +47,7 @@ x = expt.pop('rt')         # returns value for key 'rt', and removes the entry f
 x, y = expt.popitem()      # returns an arbitrary (k,v) entry, and removes it from the dictionary
 
 # shallow copy
-expt2 = expt.copy()        # values are the same, not copies; see example on pp. 64-65
+expt2 = expt.copy()        # values are identical, not copies (see Hetland, pp. 64-65)
 
 # deep copy
 from copy import deepcopy
@@ -55,4 +55,3 @@ expt2 = deepcopy(expt)
 
 # clear
 expt.clear()               # clears all entries
-
