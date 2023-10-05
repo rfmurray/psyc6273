b8942fb128432323a60de91e68f761f2e79a8d3c
Richard Murray <rfm@yorku.ca>
2023-10-05 09:51:12 -0400

    comments

--- 03 - functions; strings/text_files.py
+++ 03 - functions; strings/text_files.py
@@ -7,16 +7,19 @@ import os, random, time
 # the data file
 os.chdir('/Users/rfm/Desktop')
 
+# check current directory
+os.getcwd()
+
 # open the file
-f = open('data.txt', 'a')
+f = open('data.txt', 'a')   # could also provide a full path, e.g., '/Users/rfm/data.txt'
 # 'a' = if the file exists, append to it
 # 'w' = if the file exists, overwrite it
 # 'r' = read
 
 # write some lines to the file
 for i in range(100):
-    randnum = random.uniform(0, 1)
-    sampletime = time.time()
+    randnum = random.uniform(0, 1)  # returns a random number
+    sampletime = time.time()        # returns seconds since start of January 1, 1970 ("the epoch")
     f.write(f'{i+1:3d}, {randnum:.2f}, {sampletime:.9f}\n')
 
 # close the file
