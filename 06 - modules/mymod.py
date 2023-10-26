f700125a4616b669c4792cd37f2875772ffa2f7f
Richard Murray <rfm@yorku.ca>
2023-10-25 21:53:55 -0400

    posted lecture code

---
+++ 06 - modules/mymod.py
@@ -0,0 +1,9 @@
+# mymod.py  An illustration of how to create a module
+
+# define a function
+def addit(a=10, b=20):
+    return a+b
+
+# define a constant
+earth_radius_km = 12742
+
---
+++ 06 - modules/mymod_demo.py
@@ -0,0 +1,8 @@
+# mymod_demo.py  Import a module and use its contents
+
+import mymod as mmod
+
+mmod.addit(20,30)
+
+print(mmod.earth_radius_km)
+
