eb5d6b1f2abd9fc1d9383493b36dbbb0279ad807
Richard Murray <rfm@yorku.ca>
2024-09-04 11:14:03 -0400

    updated code

--- 01 - data types/chapter02_code.py
+++ 01 - data types/chapter02_code.py
@@ -2,16 +2,16 @@
 
 # 2.2 Variables
 
-# defining a variable
+# define a variable
 x = 5
 
-# seeing the value of a variable by typing its name at the command line
+# see the value of a variable by typing its name at the command line
 x
 
-# defining a variable with a descriptive name
+# define a variable with a descriptive name
 dot_radius = 1.5
 
-# trying to see the value of a variable that hasn't been defined;
+# try to see the value of a variable that hasn't been defined;
 # this line creates an error
 reaction_time
 
@@ -24,34 +24,34 @@ reaction_time
 #   order of operations  ( )
 (1/2) - (3+4)**2
 
-# defining variables with arithmetic operations
+# define variables with arithmetic operations
 dot_radius = 1.5
 dot_circumference = 2 * 3.1416 * 1.5
 dot_area = 3.1416 * (1.5 ** 2)
 
-# using one variable in the definition of other variables
+# use one variable in the definition of other variables
 dot_radius = 1.5
 dot_circumference = 2 * 3.1416 * dot_radius
 dot_area = 3.1416 * (dot_radius ** 2)
 
 # 2.3 Functions
 
-# calling the absolute value function
+# call the absolute value function
 u = abs(-10.5)
 
-# calling the power function
+# call the power function
 v = pow(2, 3)
 
-# calling the print function
+# call the print function
 print('Hello, world!')
 
 # 2.4 Data types
 
-# checking the type of a variable
+# check the type of a variable
 x = 5
 type(x)
 
-# checking the type of another variable
+# check the type of another variable
 y = 5.43
 type(y)
 
@@ -74,7 +74,7 @@ type(response)
 
 # 2.5 Lists
 
-# creating a list
+# create a list
 dot_position = [10, 20]
 type(dot_position)
 
@@ -83,10 +83,10 @@ some_properties = [1.5, 2, True, None, 10]
 square_circles = []
 trajectory = [[50, -10], [50, -15], [52, -18], [55, -20]]
 
-# finding the length of a list
+# fine the length of a list
 n = len(trajectory)
 
-# creating a list from other variables
+# create a list from other variables
 x = 10
 y = 20
 p = [x, y]
@@ -97,7 +97,7 @@ trajectory[0]
 trajectory[0][0]
 trajectory[0][1]
 
-# changing an element of a list
+# change an element of a list
 dot_position[1] = 5
 
 # a list method: append()
@@ -108,7 +108,7 @@ dot_position.append(30)
 primes = [2, 3, 5, 7]
 primes.extend([11, 13, 17])
 
-# comparing append() and extend()
+# compare append() and extend()
 
 test1 = [1, 2, 3]
 test1.append([4, 5, 6])
@@ -121,10 +121,11 @@ print(test2)
 # a mistake; remember that append() changes a list in place
 list1 = [10, 20, 30]
 list2 = list1.append(40)
+print(list2)
 
 # 2.6 Tuples
 
-# creating a tuple
+# create a tuple
 dot_position = (10, 20)
 dot_position = 10, 20
 type(dot_position)
@@ -133,7 +134,7 @@ type(dot_position)
 some_properties = (10, 20.5, True, (1, 2), [3, 4])
 no_properties = ()
 
-# indexing a tuple
+# index a tuple
 some_properties[1]
 
 # tuples are immutable; this generates an error
@@ -151,27 +152,20 @@ type(y)
 
 # 2.7 Strings
 
-# creating a string
+# create a string
 instruction1 = 'Use the mouse to move the white plus sign on the screen.'
 instruction2 = "Track the moving red dot as closely as you can."
 type(instruction1)
 
-# strings are immutable
+# strings are immutable; generates an error
 instruction1[0] = 'A'
 
 # string methods
 
 name = 'Target Dot'
-
 name.upper()
-Out: 'TARGET DOT'
-
 name.lower()
-Out: 'target dot'
-
 name.replace('Target', 'Red')
-Out: 'Red Dot'
-
 name.count('D')
 
 # 2.8 Sequences
@@ -184,7 +178,7 @@ len(x)
 instruction3 = 'The experiment is about to begin.'
 len(instruction3)
 
-# adding sequences
+# adding sequences concatenates them
 
 x = [1, 2, 3]
 y = [4, 5, 6]
@@ -198,14 +192,14 @@ name1 = 'Target'
 name2 = 'Dot'
 name3 = 'A Red ' + name1 + ' ' + name2
 
-# extending a tuple
+# extend a tuple
 primes = primes + (19, 23, 29)
 
-# extending a tuple by one integer
+# extend a tuple by one integer
 primes = primes + 31     # this version generates an error
 primes = primes + (31,)  # this version works
 
-# slicing a sequence
+# slice a sequence
 primes = [2, 3, 5, 7, 11, 13, 15, 17]
 primes[0:4]
 
@@ -213,7 +207,7 @@ primes[0:4]
 primes[0] + 10    # this line works
 primes[0:1] + 10  # this line generates an error
 
-# omitting endpoints in slice notation
+# can omit endpoints in slice notation
 primes[:4]
 primes[4:]
 primes[:]
