ffce57430454ef321702e28bd10145b291a81812
Richard Murray <rfm@yorku.ca>
2025-10-02 16:02:29 -0400

    added sound2 module

---
+++ 05 - numpy/sound2.py
@@ -0,0 +1,8 @@
+import sounddevice as sd
+import numpy as np
+
+def beep(freq=440, dur=0.1, amp=1):
+    fs = 44100  # sample frequency in Hz
+    t = np.linspace(0, dur, int(fs*dur))
+    wave = amp * np.sin(2*np.pi*freq*t)
+    sd.play(wave, fs)
