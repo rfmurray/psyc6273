a8146a8338a4f8bc85cbb4790f483fba3e1eb9ab
Richard Murray <rfm@yorku.ca>
2025-10-09 16:26:20 -0400

    posted problems

--- 06 - numpy etc./list_comprehensions.py
+++ 06 - numpy etc./list_comprehensions.py
@@ -21,6 +21,13 @@ numlist = [len(x) for x in strlist if len(x) > 3]
 # are more than three characters long
 shortlist = [x for x in strlist if len(x) > 3]
 
+# get a list of 100 samples from a Bernoulli random variable
+import random
+def coinflip(p=0.5):
+    return int(random.uniform(0,1) < p)
+
+[coinflip(p=0.2) for t in range(100)]
+
 # general form: [ f(varname) for [varname] in [iterable] if [conditon] ]
 # - the function f is optional
 # - the condition is optional
